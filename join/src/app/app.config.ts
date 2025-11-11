9211097e73a7aec0370c663355b4f41a009725a9
Malte <malte.schuette92@gmail.com>
2025-11-11 16:02:43 +0100

    changed firebase project number

--- join/src/app/app.config.ts
+++ join/src/app/app.config.ts
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-585ee", appId: "1:69329162459:web:f5c875c3c6aab97484fcfe", storageBucket: "join-585ee.firebasestorage.app", apiKey: "AIzaSyCokq98kJ4VnDSvOD0dUaJCzn_bmMAsvFA", authDomain: "join-585ee.firebaseapp.com", messagingSenderId: "69329162459", projectNumber: "69329162459", version: "2" })), provideFirestore(() => getFirestore())
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-585ee", appId: "1:69329162459:web:f5c875c3c6aab97484fcfe", storageBucket: "join-585ee.firebasestorage.app", apiKey: "AIzaSyCokq98kJ4VnDSvOD0dUaJCzn_bmMAsvFA", authDomain: "join-585ee.firebaseapp.com", messagingSenderId: "69329162459",})), provideFirestore(() => getFirestore())
   ]
 };
