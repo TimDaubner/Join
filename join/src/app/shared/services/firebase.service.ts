6bf978f52893b9b4ae48ea9cb13b888fe8d6f955
TimDaubner <tim.daubner@outlook.com>
2025-11-11 16:00:38 +0100

    constructor added connection to contacts TEST

--- join/src/app/shared/services/firebase.service.ts
+++ join/src/app/shared/services/firebase.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
-  
+
+  firestore = inject(Firestore);
+  unsubscribe;
+
+  constructor() {
+    this.unsubscribe = onSnapshot(collection(this.firestore, "contacts"), (contactsSnapshot) => {
+      contactsSnapshot.forEach((contact) => {
+        console.log("data :", contact.data());
+      });
+    });
+  }
+
 }
