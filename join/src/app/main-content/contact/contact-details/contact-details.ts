1b09bff5e6225f0efe3f08f80f10cd68db6860a0
Malte <malte.schuette92@gmail.com>
2025-11-12 16:31:14 +0100

    added setContractObject function, added getSingleContact function, changed service snapshot to return list

--- join/src/app/main-content/contact/contact-details/contact-details.ts
+++ join/src/app/main-content/contact/contact-details/contact-details.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { FirebaseService } from '../../../shared/services/firebase.service';
+import { Contact } from '../../../interfaces/contact.interface';
+
 
 @Component({
   selector: 'app-contact-details',
@@ -7,9 +9,22 @@ import { FirebaseService } from '../../../shared/services/firebase.service';
   templateUrl: './contact-details.html',
   styleUrl: './contact-details.scss',
 })
-export class ContactDetails {
 
+export class ContactDetails {
+firebase = inject(FirebaseService)
+contactSnap = {
+  name: "",
+  mail: "",
+  phone: "",
+};
+singleContact;
 
   constructor() {
+    this.firebase;
+   this.singleContact = this.firebase.getSingleContact('dwf1m2bluDxzy0cLlcpV')
+   console.log(this.singleContact);
+   
   }
+
+
 }
--- join/src/app/shared/services/firebase.service.ts
+++ join/src/app/shared/services/firebase.service.ts
@@ -1,23 +1,27 @@
 import { inject, Injectable } from '@angular/core';
-import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
+import { collection, Firestore, onSnapshot, doc, getDoc } from '@angular/fire/firestore';
 import { Contact } from '../../interfaces/contact.interface';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
 
-  contact: Contact[] = []
+  contact: Contact[] = [];
+  contactList: Contact[] = [];
 
   firestore: Firestore = inject(Firestore);
   unsubscribe;
 
   constructor() {
     this.unsubscribe = onSnapshot(collection(this.firestore, "contacts"), (contactsSnapshot) => {
+      this.contactList = []
       contactsSnapshot.forEach((contact) => {
-        console.log("data :", contact.data());
+        this.contactList.push(this.setContactObject(contact.id, contact.data() as Contact))
       });
     });
+    
   }
 
   ngOnDestroy() {
@@ -26,6 +30,23 @@ export class FirebaseService {
     }
   }
 
+  setContactObject(idParam:string, obj: Contact): Contact{
+    return {
+      id: idParam,
+      name: obj.name,
+      mail: obj.mail,
+      phone: obj.phone,
+    }
+  }
 
+   async getSingleContact(idParam:string) {
+    const contactRef = doc(this.firestore, 'contacts', idParam);
+    const contactSnap = await getDoc(contactRef);
+    if (contactSnap.exists()) {
+      console.log("Document Data: ", contactSnap.data());
+    } else {
+      console.log("no documentoo");
+    }
+  }
 
 }
