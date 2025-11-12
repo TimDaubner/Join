3f48b4144d527f9dbee857d87f07c7cab0a7587d
TimDaubner <tim.daubner@outlook.com>
2025-11-12 11:52:54 +0100

    added router links

--- join/src/app/app.routes.ts
+++ join/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainContent } from './main-content/main-content';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainContent},
+    // {path: 'imprint', component: Imprint},
+    // {path: 'policy', component: Policy},
+    // {path: 'help', component: Help},
+];
--- join/src/app/app.ts
+++ join/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirebaseService } from './shared/services/firebase.service';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
