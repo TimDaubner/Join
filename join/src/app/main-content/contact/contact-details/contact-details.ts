57cb51839e8a9cc4d8462a53e5cb40921f392451
Malte <malte.schuette92@gmail.com>
2025-11-13 16:49:42 +0100

    added contact details icons, added getInitials functions, added showDetails functions

--- join/src/app/main-content/contact/contact-details/contact-details.ts
+++ join/src/app/main-content/contact/contact-details/contact-details.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FirebaseService } from '../../../shared/services/firebase.service';
 import { Contact } from '../../../interfaces/contact.interface';
+import { doc, deleteDoc} from "firebase/firestore"
 
 
 @Component({
@@ -12,15 +13,36 @@ import { Contact } from '../../../interfaces/contact.interface';
 
 export class ContactDetails {
 firebase = inject(FirebaseService)
-contactSnap = {
-  name: "",
+
+contactSelected = false;
+currentContact = {
+  surname: "",
+  lastname: "",
   mail: "",
   phone: "",
 };
+initials = "";
 
   constructor() {
     this.firebase;
   }
 
+  showContactDetails($index:number){
+    this.currentContact = this.firebase.contactList[$index]
+    this.contactSelected = true;
+    this.getInitials($index);
+  }
 
+  getInitials($index:number) {
+    let firstInitial = this.firebase.contactList[$index].surname.trim().charAt(0).toUpperCase();
+    let secondInitial = this.firebase.contactList[$index].lastname.trim().charAt(0).toUpperCase();
+    this.initials = firstInitial + secondInitial;
+    console.log(this.initials);
+    console.log(this.firebase.contactList[$index]);
+    
+  }
+
+  // async deleteContactDetails($index:number) {
+  //   await deleteDoc(doc(this.firebase.firestore, this.firebase.contactList[$index].id))
+  // }
 }
