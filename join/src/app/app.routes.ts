721e2bc1ea7851f0f39f3e1b79a850cce2b0bfe6
imra.s <i.skaliks.is@gmail.com>
2025-11-15 16:26:33 +0100

    add navigation to pages

--- join/src/app/app.routes.ts
+++ join/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { MainContent } from './main-content/main-content';
-import { PrivacyPolicy } from './privacy-policy/privacy-policy';
-import { LegalNotice } from './legal-notice/legal-notice';
-import { HelpSection } from './help-section/help-section';
+import { PrivacyPolicy } from './main-content/legal/privacy-policy/privacy-policy';
+import { LegalNotice } from './main-content/legal/legal-notice/legal-notice';
+import { HelpSection } from './main-content/help-section/help-section';
+import { Summary } from './main-content/summary/summary';
+import { AddTask } from './main-content/add-task/add-task';
+import { Board } from './main-content/board/board';
 
 export const routes: Routes = [
-  { path: '', component: MainContent },
+  { path: 'contacts', component: MainContent },
+  { path: 'summary', component: Summary },
+  { path: 'add-task', component: AddTask },
+  { path: 'board', component: Board },
   { path: 'legal-notice', component: LegalNotice },
   { path: 'privacy-policy', component: PrivacyPolicy },
   { path: 'help', component: HelpSection },
--- join/src/app/main-content/main-content.ts
+++ join/src/app/main-content/main-content.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Contact } from './contact/contact';
 import { AddContactOverlay } from './add-contact-overlay/add-contact-overlay';
 import { ContactList } from './contact/contact-list/contact-list';
+import { AddTask } from './add-task/add-task';
 
 @Component({
   selector: 'app-main-content',
