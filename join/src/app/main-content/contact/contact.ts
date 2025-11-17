a805de8cac3be925a813c8c792405435ec39ac83
TimDaubner <tim.daubner@outlook.com>
2025-11-17 12:18:56 +0100

    added style for overlay

--- join/src/app/main-content/contact/contact.ts
+++ join/src/app/main-content/contact/contact.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { ContactDetails } from './contact-details/contact-details';
 import { ContactList } from './contact-list/contact-list';
+import { AddContactOverlay } from "../add-contact-overlay/add-contact-overlay";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [ContactDetails, ContactList],
+  imports: [ContactDetails, ContactList, AddContactOverlay,NgClass],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
+  isAddContactOpen = false;
 
+  openAddNewContactOverlay(){
+    this.isAddContactOpen = true;
+  }
 }
