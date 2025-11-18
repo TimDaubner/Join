4d256de07c76f672fa414f691f9ffba44a4d3b54
Malte <malte.schuette92@gmail.com>
2025-11-18 08:27:08 +0100

    cleaned up the mainPage imports

--- join/src/app/main-content/main-content.ts
+++ join/src/app/main-content/main-content.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { Contact } from './contact/contact';
-import { AddContactOverlay } from './add-contact-overlay/add-contact-overlay';
-import { ContactList } from './contact/contact-list/contact-list';
-import { EditContactOverlay } from './edit-contact-overlay/edit-contact-overlay';
-import { AddTask } from './add-task/add-task';
 
 @Component({
   selector: 'app-main-content',
-  imports: [Contact, ContactList, AddContactOverlay, EditContactOverlay],
+  imports: [Contact],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
