b09625613d01529e39e2780f8e77fbf506d173e7
imra.s <i.skaliks.is@gmail.com>
2025-11-24 22:36:44 +0100

    add empty column layout and header section

---
+++ join/src/app/interfaces/task.interface.ts
@@ -0,0 +1,12 @@
+import { Timestamp } from '@angular/fire/firestore';
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  priority: string;
+  status: string;
+  duedate: Timestamp;
+  assignees: string[];
+}
--- join/src/app/main-content/board/board.ts
+++ join/src/app/main-content/board/board.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BoardCard } from "./board-card/board-card";
+import { BoardCard } from './board-card/board-card';
+import { Task } from './../../interfaces/task.interface';
 
 @Component({
   selector: 'app-board',
   standalone: true,
   imports: [CommonModule, BoardCard],
   templateUrl: './board.html',
-  styleUrl: './board.scss',
+  styleUrls: ['./board.scss', './board-card/board-card.scss'],
 })
-export class Board {}
+export class Board {
+  columns: { title: string; tasks: Task[] }[] = [
+    { title: 'To do', tasks: [] },
+    { title: 'In progress', tasks: [] },
+    { title: 'Await feedback', tasks: [] },
+    { title: 'Done', tasks: [] },
+  ];
+}
