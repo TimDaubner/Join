58d5059044deba62d1bffbd3959b47a2f2f773e5
Malte <malte.schuette92@gmail.com>
2025-11-25 18:43:38 +0100

    changed to mat-design-form

--- join/src/app/main-content/add-task/add-task.ts
+++ join/src/app/main-content/add-task/add-task.ts
@@ -1,19 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgModel, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Task} from '../../interfaces/task.interface'
 import { Timestamp } from '@angular/fire/firestore';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { FirebaseService } from '../../shared/services/firebase.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatInputModule, MatSelectModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatIconModule
+  ],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
+  providers: [
+    provideNativeDateAdapter()
+  ]
 })
 export class AddTask {
+
+  firebase = inject(FirebaseService)
 
   newTask: Task = {
     title: "",
