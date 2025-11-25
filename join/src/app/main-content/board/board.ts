c35e3fca322b85af12957912fbdb12ed05ef1c81
imra.s <i.skaliks.is@gmail.com>
2025-11-25 12:43:38 +0100

    add angular drag-drop

--- join/src/app/main-content/board/board.ts
+++ join/src/app/main-content/board/board.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BoardCard } from './board-card/board-card';
 import { Task } from './../../interfaces/task.interface';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CommonModule, BoardCard],
+  imports: [CommonModule, CdkDrag],
   templateUrl: './board.html',
   styleUrls: ['./board.scss', './board-card/board-card.scss'],
 })
