0de135451d91c2c564873fd8a1b55073a29095a4
imra.s <i.skaliks.is@gmail.com>
2025-11-26 10:48:16 +0100

    add changes

--- join/src/app/interfaces/task.interface.ts
+++ join/src/app/interfaces/task.interface.ts
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import { Timestamp } from "@angular/fire/firestore";
-
-export interface Task {
-    assignedTo: string;
-    columnCategory:string;
-    description:string;
-    dueDate:Date | Timestamp;
-    priority:string;
-    subTask:Subtask;
-    taskCategory:string;
-    titel:string;
-}
-
-export interface Subtask {
-    test?:string,
-    tester?:boolean
-}
-=======
 import { Timestamp } from '@angular/fire/firestore';
 
 export interface Task {
@@ -36,4 +17,3 @@ export interface Subtask {
   title: string;
   isdone: boolean;
 }
->>>>>>> imra
