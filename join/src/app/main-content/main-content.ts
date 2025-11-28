cf4c8b281908d50fc7343eeb2f91ccc6b2d6d992
Malte <malte.schuette92@gmail.com>
2025-11-28 10:22:30 +0100

    removed unused imports, changed input css add task component

--- join/src/app/main-content/main-content.ts
+++ join/src/app/main-content/main-content.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { Contact } from './contact/contact';
-import { Board } from './board/board';
-import { AddTask } from './add-task/add-task';
 
 @Component({
   selector: 'app-main-content',
-  imports: [Contact,Board,AddTask],
+  imports: [Contact],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
