24cf08b5e15c95f8becdfcf79f5a8f4c44191efc
imra.s <i.skaliks.is@gmail.com>
2025-11-30 18:58:12 +0100

    add component edit card details

---
+++ join/src/app/main-content/board/board-card/edit-card-details/edit-card-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-card-details',
+  imports: [],
+  templateUrl: './edit-card-details.html',
+  styleUrl: './edit-card-details.scss',
+})
+export class EditCardDetails {
+
+}
