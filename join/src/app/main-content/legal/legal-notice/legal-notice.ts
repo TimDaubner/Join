4f49a0c743669771443d9cbfab798818707d04d3
imra.s <i.skaliks.is@gmail.com>
2025-12-10 17:24:31 +0100

    add legal notice

--- join/src/app/main-content/legal/legal-notice/legal-notice.ts
+++ join/src/app/main-content/legal/legal-notice/legal-notice.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
-export class LegalNotice {}
+export class LegalNotice {
+  constructor(private location: Location) {}
+
+  goBack() {
+    this.location.back();
+  }
+}
