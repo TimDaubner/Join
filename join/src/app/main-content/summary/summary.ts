05b4b2e158f34a201c6abb8f5d0047096dbe39ac
Malte <malte.schuette92@gmail.com>
2025-12-12 15:16:58 +0100

    Implement task priority display and upcoming deadline logic in summary component

--- join/src/app/main-content/summary/summary.ts
+++ join/src/app/main-content/summary/summary.ts
@@ -12,6 +12,8 @@ export class Summary {
 
 boardService = inject(BoardService)
 
+highestPrioTask = "";
+
 urgentTasks: Task[] = [];
 mediumTasks: Task[] = [];
 lowTasks: Task[] = [];
@@ -22,7 +24,6 @@ constructor() {
 
 fillTaskLists() {
   for(let i= 0; i < this.boardService.taskList.length; i++) {
-    
     if(this.boardService.taskList[i].priority == 'Low') {
       this.lowTasks.push(this.boardService.taskList[i]);
     } else if (this.boardService.taskList[i].priority == 'Medium') {
@@ -30,10 +31,40 @@ fillTaskLists() {
     } else if (this.boardService.taskList[i].priority == 'Urgent') {
       this.urgentTasks.push(this.boardService.taskList[i]);
     }
-  }
-
-  
-    
+  }    
 }
+
+getHighestPrioTask() {
+  if (this.urgentTasks) {
+    this.highestPrioTask = "Urgent"
+    return this.getNextDueDate(this.urgentTasks)
+  }
+  else if (!this.urgentTasks) {
+    this.highestPrioTask = "Medium"
+    return this.getNextDueDate(this.mediumTasks)
+  }
+  else if (!this.urgentTasks && !this.mediumTasks) {
+    this.highestPrioTask = "Low"
+    return (this.getNextDueDate(this.lowTasks))
+  }
+  else {
+    return "There are no open Tasks with deadlines"
+  }
+}
+
+getNextDueDate(prioList: Task[]) {
+  let closestDeadline = prioList[0].dueDate;
+  let taskTitle = "";
+    for (let i = 1; i < prioList.length; i++) {
+      if (closestDeadline > prioList[i].dueDate) {
+        closestDeadline = prioList[i].dueDate;
+        taskTitle = prioList[i].title
+      }
+  }
+  console.log("nächste Deadline: " + closestDeadline.toDate() + "für " + taskTitle );
+  return closestDeadline.toDate().toLocaleDateString('en-UK');
+}
+
+
 
 }
