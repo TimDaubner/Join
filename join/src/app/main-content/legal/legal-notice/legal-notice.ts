5c89f28825ed8f0675ece4df99653b7866f5cd81
imra.s <i.skaliks.is@gmail.com>
2025-12-14 13:22:22 +0100

    hide arrow when not logged in

--- join/src/app/main-content/legal/legal-notice/legal-notice.ts
+++ join/src/app/main-content/legal/legal-notice/legal-notice.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-legal-notice',
@@ -10,7 +11,7 @@ import { Location } from '@angular/common';
   styleUrl: './legal-notice.scss',
 })
 export class LegalNotice {
-  constructor(private location: Location) {}
+  constructor(private location: Location, public authService: AuthService) {}
 
   goBack() {
     this.location.back();
--- join/src/app/main-content/legal/privacy-policy/privacy-policy.ts
+++ join/src/app/main-content/legal/privacy-policy/privacy-policy.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -10,7 +11,7 @@ import { Location } from '@angular/common';
   styleUrl: './privacy-policy.scss',
 })
 export class PrivacyPolicy {
-  constructor(private location: Location) {}
+  constructor(private location: Location, public authService: AuthService) {}
 
   goBack() {
     this.location.back();
