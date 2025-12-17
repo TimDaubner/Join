0d023976a8dbe64e65d2e3e57586329007078de5
Malte <malte.schuette92@gmail.com>
2025-12-17 12:50:14 +0100

    Summary Responsive

--- join/src/app/main-content/summary/summary.ts
+++ join/src/app/main-content/summary/summary.ts
@@ -93,11 +93,11 @@ setGreeting() {
     const hour = new Date().getHours();
     
     if (hour < 12) {
-      this.greeting = "Good morning";
+      this.greeting = "Good morning,";
     } else if (hour < 18) {
-      this.greeting = "Good afternoon";
+      this.greeting = "Good afternoon,";
     } else {
-      this.greeting = "Good evening";
+      this.greeting = "Good evening,";
     }
   }
 
